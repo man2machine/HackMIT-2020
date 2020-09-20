5c9f65b40d25ba55b278e12271528c8fe2d79a53
Sathwik Karnik <skarnik2012@gmail.com>
2020-09-19 23:27:08 -0400

    minor fixes to make code more modular

--- hackmit2020/timeseries.py
+++ hackmit2020/timeseries.py
@@ -7,9 +7,6 @@ Created on Sat Sep 19 23:19:33 2020
 
 import json
 import geojson as gj
-import numpy as np
-import statsmodels.api as sm
-from statsmodels.tsa.arima_model import ARIMA
 
 HOURS_PER_DAY = 24
 
@@ -21,28 +18,23 @@ def compute_forecasted_visitors_per_hour(hours_per_day_per_week):
     
     return [a/(len(hours_per_day_per_week) / HOURS_PER_DAY) for a in avg_results_per_day]
     
-def get_avg_num_visitors_per_hour_each_week(data):
-    num_visitors_per_hour = []
-    
-    for row in data:
-        avg_results_per_day = [0 for i in range(HOURS_PER_DAY)]
-        str_visits_by_each_hour = row['record']['visits_by_each_hour']
-        if len(str_visits_by_each_hour) != 0:
-            hours_per_day_per_week = json.loads(str_visits_by_each_hour)
-            avg_results_per_day = compute_forecasted_visitors_per_hour(hours_per_day_per_week)
+def get_avg_num_visitors_per_hour_each_week(entry):
+    avg_results_per_day = [0 for i in range(HOURS_PER_DAY)]
+    str_visits_by_each_hour = entry['record']['visits_by_each_hour']
+    if len(str_visits_by_each_hour) != 0:
+        hours_per_day_per_week = json.loads(str_visits_by_each_hour)
+        avg_results_per_day = compute_forecasted_visitors_per_hour(hours_per_day_per_week)
         
-        num_visitors_per_hour.append([a for a in avg_results_per_day])
-        
-    return num_visitors_per_hour
+    return [a for a in avg_results_per_day]
 
-def get_weekly_visit_geojson(data, num_visitors_per_hour):
+def get_weekly_visit_geojson(data):
     features = []
     for i in range(len(data)):
         entry = data[i]
-        hourly_forecast = num_visitors_per_hour[i]
+        hourly_forecast = get_avg_num_visitors_per_hour_each_week(entry)
         props = {
             "hourly_forecasts": hourly_forecast,
-            "hours in the day": [j for j in range(len(hourly_forecast))],
+            "hours_in_the_day": [j for j in range(len(hourly_forecast))],
         }
         coords = (float(entry["mapping"]["longitude"]), float(entry["mapping"]["latitude"]))
         feat = gj.Feature(
