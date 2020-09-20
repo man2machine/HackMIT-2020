f5a1d65267fe8bc7a7fe5a226b4329437841f4b6
Sathwik Karnik <skarnik2012@gmail.com>
2020-09-20 04:50:47 -0400

    more relevant stats; this time, for each category

--- hackmit2020/relevant_stats.py
+++ hackmit2020/relevant_stats.py
@@ -15,3 +15,39 @@ def get_median_dwell_times(data):
         median_dwell_times.append(median_dwell_time)
     
     return median_dwell_times
+
+def get_categories(data):
+    categories = set()
+    
+    for entry in data:
+        categories.add(entry['mapping']['top_category'])
+    
+    return list(categories)
+
+def get_stats_from_each_category(data):
+    """
+    This function returns the stats from each category
+
+    """
+    
+    categories = get_categories(data)
+    mean_of_median_dwell_times = {categories[i]:0 for i in range(len(categories))}
+    mean_raw_visitor_counts = {categories[i]:0 for i in range(len(categories))}
+    count = {categories[i]:0 for i in range(len(categories))}
+    
+    for entry in data:
+        for category in categories:
+            record_entry = entry['record']
+            mapping_entry = entry['mapping']
+            entry_category = mapping_entry['top_category']
+            if entry_category and entry_category == category:
+                mean_of_median_dwell_times[category] += float(record_entry['median_dwell'])
+                mean_raw_visitor_counts[category] += int(record_entry['raw_visit_counts'])
+                count[category] += 1
+        
+    for category in categories:
+        if count[category] > 0:
+            mean_of_median_dwell_times[category] = mean_of_median_dwell_times[category]/count[category]
+            mean_raw_visitor_counts[category] = mean_raw_visitor_counts[category]/count[category]
+    
+    return {"mean_median_dwell": mean_of_median_dwell_times, "mean_raw_visitor_counts": mean_raw_visitor_counts}
