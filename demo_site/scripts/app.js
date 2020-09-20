17466b027158d5073209a20211e4ab56835d8974
Muhammad Shahir Rahman <shahir2000@gmail.com>
2020-09-20 08:38:41 -0700

    decent version

--- demo_site/scripts/app.js
+++ demo_site/scripts/app.js
@@ -12,8 +12,14 @@ var map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 map.dragPan.enable();
 
+var current_day = 0;
+var p_date = new Date();
+var current_hour = p_date.getHours();
+
+var text_dates = ['9/7', '9/8', '9/9', '9/10', '9/11', '9/12', '9/13', '9/14'];
+
 function add_visits_map() {
-    map.addSource('test', {
+    map.addSource('visits_count', {
         type: 'geojson',
         data: '../resources/analytics_cache/09-16-2020-weekly-visit-counts-map.geojson'
     });
@@ -21,7 +27,7 @@ function add_visits_map() {
     map.addLayer({
         'id': 'heatmap',
         'type': 'circle',
-        'source': 'test',
+        'source': 'visits_count',
         'paint': {
             'circle-radius': {
                 'base': 1.75,
@@ -33,6 +39,8 @@ function add_visits_map() {
             // https://www.schemecolor.com/red-orange-green-gradient.php
             'circle-color': [
                 'case',
+                ["<", ['get', 'index'], -1],
+            '#cccccc',
                 [">", ['get', 'week_visits'], 50],
                 green_red_colormap[5],
                 [">", ['get', 'week_visits'], 40],
@@ -51,6 +59,231 @@ function add_visits_map() {
     });
 }
 
+function get_locs_metadata(indices, on_success, model_forecast, single_hour) {
+    var model_type;
+    if (model_forecast) {
+        model_type = "model";
+    }
+    else {
+        model_type = "avg";
+    }
+    var forecast_hour = null;
+    if (single_hour) {
+        forecast_hour = current_hour;
+    }
+    console.log(JSON.stringify({ "query_type": "record_detailed_data", "indices": indices, "forecast_model": model_type, "forecast_hour": forecast_hour }));
+    var response = $.ajax({
+        url: 'http://localhost:5000/',
+        type: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        crossDomain: true,
+        cache: false,
+        xhrFields: {
+            withCredentials: false
+        },
+        data: JSON.stringify({ "query_type": "record_detailed_data", "indices": indices, "forecast_model": model_type, "forecast_hour": forecast_hour }),
+        dataType: 'json',
+        success: on_success
+    });
+}
+
+var lower_line_layout = {
+    height: 200,
+    margin: {
+        l: 1,
+        r: 1,
+        b: 1,
+        t: 1,
+        pad: 4
+    }
+};
+
+var forecast_graph_elem = document.getElementById('forecast_graph');
+var trace = {
+    x: [],
+    y: [],
+    name: 'name',
+    type: 'scatter'
+};
+Plotly.newPlot(forecast_graph_elem, [trace], lower_line_layout)
+
+function show_forecast_graph(metadata) {
+    var y_data_past = [];
+    var i = current_hour;
+    while (true) {
+        if (i >= metadata.visits_by_each_hour.length) {
+            break;
+        }
+        y_data_past.push(metadata.visits_by_each_hour[i]);
+        i += 24;
+    }
+    var y_data_forecast = [];
+    y_data_forecast.push(y_data_past[y_data_past.length - 1]);
+    y_data_forecast.push(metadata.forecast_info)
+    var trace1 = {
+        x: text_dates.slice(0, 6),
+        y: y_data_past,
+        name: 'data',
+        type: 'scatter'
+    };
+    var trace2 = {
+        x: text_dates.slice(5, 7),
+        y: y_data_forecast,
+        name: 'forecast',
+        type: 'scatter'
+    };
+    var layout = {
+        height: 200,
+        margin: {
+            l: 30,
+            r: 5,
+            b: 30,
+            t: 5,
+            pad: 4
+        },
+    };
+    var traces = [trace1, trace2];
+    console.log(JSON.stringify(traces))
+    Plotly.react(forecast_graph_elem, traces, layout);
+}
+
+var visit_graph_elem = document.getElementById('visit_graph');
+var trace = {
+    x: [],
+    y: [],
+    name: 'name',
+    type: 'bar'
+};
+Plotly.newPlot(visit_graph_elem, [trace], lower_line_layout);
+
+function show_visits_graph(metadata) {
+    var y_data = metadata.visits_by_day;
+    var trace1 = {
+        x: text_dates.slice(0, 7),
+        y: y_data,
+        name: 'data',
+        type: 'bar'
+    };
+    var layout = {
+        height: 200,
+        margin: {
+            l: 30,
+            r: 5,
+            b: 30,
+            t: 5,
+            pad: 4
+        },
+    };
+    var traces = [trace1];
+    console.log(JSON.stringify(traces))
+    Plotly.react(visit_graph_elem, traces, layout);
+}
+
+function get_nearest_locs_metadata(loc_metadata, on_success) {
+    var response = $.ajax({
+        url: 'http://localhost:5000/',
+        type: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        crossDomain: true,
+        cache: false,
+        xhrFields: {
+            withCredentials: false
+        },
+        data: JSON.stringify({ "query_type": "closest_locs",  "longitude": loc_metadata.latitude, "latitude": loc_metadata.longitude, "return_metadata": true }),
+        dataType: 'json',
+        success: on_success
+    });
+}
+
+function update_table(locs_metadatas) {
+    $('#results-table-body').empty();
+    for (var i = 0; i < 4; i++) {
+        var entry = locs_metadatas["metadatas"][i];
+        var index = locs_metadatas["indices"][i];
+        var html = "";
+        html += "<tr id=\"table_entry"+JSON.stringify(i)+"\">";
+        html += "<td>"+entry.location_name+"</td>";
+        html += "<td>"+entry.top_category+"</td>";
+        html += "<td>"+entry.street_address+"</td>";
+        html += "</tr>"
+        $('#results-table-body').prepend(html);
+    }
+}
+
+var cat_dwell_graph_elem = document.getElementById('category_dwell_graph');
+
+$.getJSON("../resources/analytics_cache/09-16-2020-weekly-category-stats.json", function(data) {
+    var categories = [];
+    var times = [];
+    for (var key in data) {
+        categories.push(key);
+        times.push(data[key]["mean_median_dwell"]);
+    }
+    var layout = {
+        height: 200,
+        margin: {
+            l: 30,
+            r: 5,
+            b: 30,
+            t: 5,
+            pad: 4
+        },
+        yaxis: {
+            showticklabels: false
+        },
+        barmode: 'group'
+    };
+    var trace = {
+        x: times,
+        y: categories,
+        name: 'name',
+        type: 'bar',
+        orientation: 'h'
+    };
+    Plotly.newPlot(cat_dwell_graph_elem, [trace], layout);
+});
+
 map.on('load', function () {
     add_visits_map();
+
+    map.on('click', 'heatmap', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var properties = e.features[0].properties;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        function on_success1(data) {
+            console.log(JSON.stringify(data[0]))
+            desc = "<strong>" + data[0].location_name + "</strong>";
+            desc += "<p>";
+            desc += data[0].street_address + ", " + data[0].city + ", " + data[0].region + ", " + data[0].postal_code;
+            desc += "<br>";
+            desc += "</p>";
+            new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(desc)
+                .addTo(map);
+            show_forecast_graph(data[0]);
+            show_visits_graph(data[0]);
+
+            function on_success2(locs_metadata) {
+                console.log(JSON.stringify(locs_metadata));
+                update_table(locs_metadata)
+            }
+
+            get_nearest_locs_metadata(data[0], on_success2);
+        }
+        get_locs_metadata([properties.index], on_success1, true, true);
+
+        
+    });
 });
--- hackmit2020/timeseries.py
+++ hackmit2020/timeseries.py
@@ -20,37 +20,45 @@ def compute_avg_forecasted_visitors_per_hour(hours_per_day_per_week):
     
     return [a/(len(hours_per_day_per_week) / HOURS_PER_DAY) for a in avg_results_per_day]
     
-def get_avg_forecast_visitors_per_hour(entry):
+def get_avg_forecast_visitors_per_hour(entry, forecast_hour=None):
     avg_results_per_day = [0 for i in range(HOURS_PER_DAY)]
     str_visits_by_each_hour = entry['record']['visits_by_each_hour']
     if len(str_visits_by_each_hour) != 0:
         hours_per_day_per_week = json.loads(str_visits_by_each_hour)
         avg_results_per_day = compute_avg_forecasted_visitors_per_hour(hours_per_day_per_week)
         
-    return [a for a in avg_results_per_day]
+    hour_preds = [a for a in avg_results_per_day]
+    if forecast_hour is not None:
+        return hour_preds[forecast_hour]
+    return hour_preds
 
-def get_model_forecast_visitors_per_hour(entry):
+def get_model_forecast_visitors_per_hour(entry, forecast_hour=None):
     hour_preds = []
     hours_data = json.loads(entry['record']['visits_by_each_hour'])
-    for hour in range(HOURS_PER_DAY):
+    def get_hour_pred(hour):
         visits_data = []
         i = hour
         while True:
             if i >= len(hours_data):
                 break
             visits_data.append(hours_data[i])
-            i += 7
-        visits_data = visits_data*2
-        order = compute_best_param(visits_data, range(5), range(2), range(5))
+            i += HOURS_PER_DAY
+        #visits_data = visits_data*2
+        order = compute_best_param(visits_data, range(2), range(1), range(2))
         next_pred = compute_model_forecasted_visitors_per_hour(visits_data, order)
-        hour_preds.append(next_pred)
+        return next_pred
+    if forecast_hour is not None:
+        return get_hour_pred(forecast_hour)
+    for hour in range(HOURS_PER_DAY):
+        hour_preds.append(get_hour_pred(hour))
     return hour_preds
 
-def get_hourly_forecast(entry, method="model"):
+def get_hourly_forecast(entry, method="avg", forecast_hour=None):
+    print(entry, method)
     if method == "avg":
-        hourly_forecast = get_avg_forecast_visitors_per_hour(entry)
+        hourly_forecast = get_avg_forecast_visitors_per_hour(entry, forecast_hour=forecast_hour)
     elif method == "model":
-        hourly_forecast = get_model_forecast_visitors_per_hour(entry)
+        hourly_forecast = get_model_forecast_visitors_per_hour(entry, forecast_hour=forecast_hour)
     else:
         raise ValueError()
     return hourly_forecast
